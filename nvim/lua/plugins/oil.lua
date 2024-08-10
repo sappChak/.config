5d0f23cd88ebfc176679ce68e0353d5ca09b6517
Andrii Konotop <konotop401@gmail.com>
2024-08-10 08:58:15 +0200

    feat(oil.nvim): enhance configuration with additional options

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,8 +1,24 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
-	-- Optional dependencies
-	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	dependencies = { "nvim-tree/nvim-web-devicons" }
-  --
+	opts = {
+		default_file_explorer = true,
+		delete_to_trash = true,
+		columns = {
+			"icon",
+			-- "permissions",
+			-- "size",
+			-- "mtime",
+		},
+		float = {
+			padding = 1,
+		},
+		view_options = {
+			-- Show files and directories that start with "."
+			show_hidden = false,
+			is_always_hidden = function(name, _)
+				return name == ".. " or name == ".git"
+			end,
+		},
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
